9eb4ee8bcf3e3236b18987e029956e452de3f8ff
nlodx <lamer.midget@gmail.com>
2017-03-27 11:01:53 -0700

    Update upgrayyd.sh

--- upgrayyd.sh
+++ upgrayyd.sh
@@ -5,10 +5,9 @@ echo
 echo "Upgrayyd 0.1 by nlod"
 echo
 echo
-
 sudo -- sh -c 'apt-get update; apt-get upgrade -y; apt-get dist-upgrade -y; apt-get autoremove -y'
 echo
 echo
-echo
 echo "Updating has been completed. Thanks!"
+echo
 echo
